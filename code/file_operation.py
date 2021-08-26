ae59e4623511e830647a82b9333137e53f7b60f7
Ankit Chandel <ankit.chandel@ad.infosys.com>
2021-08-26 13:06:52 +0530

    remove path

--- code/file_operation.py
+++ code/file_operation.py
@@ -6,7 +6,7 @@ import os
 # relative_path = r'\python_handson\nfs.example.lan.yml'
 # path = os.path.join(sf.data_path,relative_path)
 relative_path = 'nfs.example.lan.yml'
-path = r'C:\Users\ankit.chandel\Desktop\python_handson\nfs.example.lan.yml'
+# path = r'C:\Users\ankit.chandel\Desktop\python_handson\nfs.example.lan.yml'
 with open(relative_path, 'r') as stream:
     try:
         yaml_content = yaml.safe_load(stream)
